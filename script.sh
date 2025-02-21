e26a5be3a48e5afacbc6c518c3249827b9d9ca60
Fábio <fabiomss2@outlook.com>
2025-02-21 16:57:22 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ sudo yum install git -y
 sudo git clone https://github.com/Fabiomss/Desafio-linux  /var/www/html/desafio-linux
 sudo mv /var/www/html/desafio-linux/*  /var/www/html
 sudo systemctl restart httpd
-curl -x POST -d "nome=Fabio M S Souza" https://difusaotech.com.br/lab/aws/index.php
+sudo curl -x POST -d "nome=Fabio M S Souza" https://difusaotech.com.br/lab/aws/index.php
 echo "Script executado com sucesso em $(date) >> /var/log/setup-script.log
